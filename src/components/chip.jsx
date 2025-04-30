e3e3c952b1a1fe90c9fef3bd54fe7df8e7cdc20a
vikasvishwagna <vishwagnavikassai@gmail.com>
2025-04-30 14:00:39 +0530

    fix:resolved issue in chip component

--- src/components/chip.jsx
+++ src/components/chip.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
 
 function chip({data, selectedMovies, setSelectedMovies}) {
   function handleClick(){
@@ -18,7 +21,7 @@ function chip({data, selectedMovies, setSelectedMovies}) {
       {data}
 
       <div style={{cursor:"pointer"}} onClick={handleClick}>
-        X
+      <FontAwesomeIcon icon={faTimes} />
       </div>
     </div>
   )
